082c8498120a77d8e6634a71b4f2733672eea1f0
jixinbao <jixinbao@dcloud.io>
2023-12-15 20:24:16 +0800

    Update base path in vite.config.ts

--- uni-app-version/vite.config.ts
+++ uni-app-version/vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/uvm2",
+  base: "/uvm2/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
